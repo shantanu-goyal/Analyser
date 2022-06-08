8fcd30b7f4889f8fdd4b4f6e843daf3b35622a48
Shantanu Goyal <shantanu.goyal@sprinklr.com>
2022-06-08 09:00:31 +0530

    Updated Navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,9 +1,23 @@
-import React from "react";
+import React, { useRef } from "react";
 import { NavLink } from "react-router-dom";
 import '../styles/NavBar.css';
 
 
+
+
 export const NavBar = function () {
+
+  const navRef = useRef(null);
+
+  function handleNavItemClick(e) {
+    e.preventDefault();
+    if (navRef.current.className === "topnav") {
+      navRef.current.className += " responsive";
+    } else {
+      navRef.current.className = "topnav";
+    }
+  }
+
   const navLinkStyles = ({ isActive }) => {
     return {
       fontWeight: isActive ? 'bold' : 'normal'
@@ -11,7 +25,7 @@ export const NavBar = function () {
   }
 
   return (
-    <nav className="primary-nav">
+    <div ref={navRef} className="topnav" id="myTopNav">
       <NavLink style={navLinkStyles} to='/'>Home</NavLink>
       <NavLink style={navLinkStyles} to='/bootup-time'>Bootup Time</NavLink>
       <NavLink style={navLinkStyles} to='/mainthread-work-breakdown'>Main Thread Work BreakDown</NavLink>
@@ -20,7 +34,8 @@ export const NavBar = function () {
       <NavLink style={navLinkStyles} to='/network-server-latency'>Network Server Latency</NavLink>
       <NavLink style={navLinkStyles} to='/resource-summary'>Resource Summary</NavLink>
       <NavLink style={navLinkStyles} to='/third-party-summary'>Third Party Summary</NavLink>
-    </nav >
+      <NavLink style={navLinkStyles} to='#' className="icon" onClick={handleNavItemClick}><i className="fa fa-bars"></i></NavLink>
+    </div>
   )
 }
 
--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/App';
 import { BrowserRouter } from 'react-router-dom';
+import "./styles/Index.css"
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
