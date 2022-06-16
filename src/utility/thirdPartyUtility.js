2b6a883873c27becd9067c4be0673602f865b21f
Shantanu Goyal <shantanu.goyal@sprinklr.com>
2022-06-16 16:11:58 +0530

    Added comments to thirdPartyUtility

--- src/utility/thirdPartyUtility.js
+++ src/utility/thirdPartyUtility.js
@@ -78,42 +78,82 @@ function getHostname(url){
 }
 
 
+
+/**
+ * Function transforms the data 
+ * 
+ * @param {object} data - Object containing metrics of all scripts
+ * @returns {object} - Scripts, Third Party Scripts, Scripts which are not third party and available for the user to mark, Entities and their metrics, mapping of entity to different scripts
+ */
 function transformData(data) {
     let items = data.details;
+
+    // Getting all scripts from the data
     const scripts = items.map(item => {
       return {
         url: item[0],
         data: item[1]
       }
     })
+
+    // Initialising third party script array
     const thirdPartyScripts = [];
+    
+    // Initialising a map mapping entity to its metrics
     const byEntity = new Map();
+    
+    // Initialising a map mapping entity to its related scripts
     const entityWiseScripts=new Map();
+    
+    // Map mapping domains that are not third party
     const domains=new Map();
+
     scripts.forEach(script => {
+      // Get the hostname for each script 
       let scriptURL = getHostname(script.url);
       if(!scriptURL){
-        return {};
+        return;
       }
+      
+      // Check if entity is present in the third party web database
       let entity = thirdPartyWeb.getEntity(scriptURL);
+      
+      // Get the data from the script (metrics)
       let scriptData = script.data;
+      
+      // Default metrics for entity 
       const defaultConfig = {
         mainThreadTime: 0,
         blockingTime: 0,
         transferSize: 0
       }
+      
       if (entity) {
+        // Push current script into third party script array 
         thirdPartyScripts.push(script);
+        
+        // Check if we have metrics related to that entity
         const currentEntity = byEntity.get(entity.name) || { ...defaultConfig };
+        
+        // Check if we have scripts related to that entity
         const scriptForEntity=entityWiseScripts.get(entity.name)||[];
+        
+        // Push the current script in the scripts related to the entity 
         scriptForEntity.push(script.url);
+        
+        // Update the scripts in the map
         entityWiseScripts.set(entity.name, scriptForEntity);
+        
+        // Update the metrics
         currentEntity.mainThreadTime += scriptData.mainThreadTime;
         currentEntity.blockingTime += scriptData.blockingTime;
         currentEntity.transferSize += scriptData.transferSize;
+
+        // Update the metrics in the map
         byEntity.set(entity.name, currentEntity);
       }
       else{
+        // Mark it as a dropdown script
         domains.set(scriptURL,1);
       }
     })
