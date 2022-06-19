a3a45c26329d88211f90b27d5831b832644cc160
Ayush Kumar Gupta <ayush.gupta@sprinklr.com>
2022-06-19 11:56:57 +0530

    Fix multi domains under same entity bug

--- src/pages/Insights.js
+++ src/pages/Insights.js
@@ -19,53 +19,69 @@ export default function Insights() {
   const thirdPartyData = dataContext.data.thirdParty;
   const config = dataContext.data.config;
 
-  function getThirdPartyData() {
-    const thirdPartyWithNetwork = thirdPartyData.map((item) => {
-      let numValidSubIems = 0
-      let summary = {
-        url: "Summary",
-        mainThreadTime: 0,
-        blockingTime: 0,
-        transferSize: 0,
-        resourceSize: 0,
-        minified: "Yes",
-        unusedPercentage: 0
-      };
-      item.subItems.items.forEach((subitem) => {
-        if (typeof subitem.url !== "string") return;
-        if(unminifiedJSData.details.items.find(({url}) => url === subitem.url)) {
-          subitem.minified = "No"
-          summary.minified = "No"
-        }
-        else subitem.minified = "Yes"
-        let js = unusedJSData.details.items.find(({url}) => url === subitem.url)
-        if(js){
-          subitem.unusedPercentage = js.wastedPercent
-          summary.unusedPercentage += js.wastedBytes
-        }
-        else subitem.unusedPercentage = 0
-        summary.mainThreadTime += subitem.mainThreadTime;
-        summary.blockingTime += subitem.blockingTime;
-        summary.transferSize += subitem.transferSize;
-        summary.resourceSize += subitem.resourceSize;
-        numValidSubIems += 1
-      });
-      // item.subItems.items.push(summary)
-      summary.unusedPercentage  = (summary.unusedPercentage / summary.transferSize) * 100;
-      
-      let opportunities = getOpportunities(summary, numValidSubIems);
-      return {
-        ...item,
-        subItems: {
-          ...item.subItems,
-          items: [...item.subItems.items, summary],
-        },
-        opportunities
-      };
+  const thirdPartyWithNetwork = thirdPartyData.reduce((acc, item) => {
+    if(!item.entityName) return acc;
+    let prevItem = acc.find(({entityName}) => item.entityName === entityName)
+    let summary = {
+      url: "Summary",
+      mainThreadTime: 0,
+      blockingTime: 0,
+      transferSize: 0,
+      resourceSize: 0,
+      minified: "Yes",
+      unusedPercentage: 0,
+    };
+    let newItems = []
+    item.subItems.items.forEach((subitem) => {
+      if (typeof subitem.url !== "string") return;
+      if (
+        unminifiedJSData.details.items.find(({ url }) => url === subitem.url)
+      ) {
+        subitem.minified = "No";
+        summary.minified = "No";
+      } else subitem.minified = "Yes";
+      let js = unusedJSData.details.items.find(
+        ({ url }) => url === subitem.url
+      );
+      if (js) {
+        subitem.unusedPercentage = js.wastedPercent;
+        summary.unusedPercentage += js.wastedBytes;
+      } else subitem.unusedPercentage = 0;
+      summary.mainThreadTime += subitem.mainThreadTime;
+      summary.blockingTime += subitem.blockingTime;
+      summary.transferSize += subitem.transferSize;
+      summary.resourceSize += subitem.resourceSize;
+      newItems.push(subitem);
     });
+    // item.subItems.items.push(summary)
+    if(prevItem){
+      let prevNumSubItems = prevItem.subItems.items.length;
+      let prevSummary = prevItem.subItems.items[prevNumSubItems - 1]
+      summary.mainThreadTime += prevSummary.mainThreadTime;
+      summary.blockingTime += prevSummary.blockingTime;
+      summary.transferSize += prevSummary.transferSize;
+      summary.resourceSize += prevSummary.resourceSize;
+      summary.unusedPercentage =
+      ((( (prevSummary.unusedPercentage * prevSummary.transferSize)/100 ) + summary.unusedPercentage) / summary.transferSize) * 100;
+      let opportunities = getOpportunities(summary, newItems.length + prevNumSubItems - 1);
+      prevItem.subItems.items.pop()
+      prevItem.subItems.items = [...prevItem.subItems.items , ...newItems, summary]
+      prevItem.opportunities = opportunities
+      return acc
+    }
+    summary.unusedPercentage =
+      (summary.unusedPercentage / summary.transferSize) * 100;
 
-    return thirdPartyWithNetwork;
-  }
+    let opportunities = getOpportunities(summary, newItems.length )
+    return [...acc, {
+      ...item,
+      subItems: {
+        ...item.subItems,
+        items: [...item.subItems.items, summary],
+      },
+      opportunities,
+    }];
+  }, []);
 
   const headings = [
     { key: "url", text: "URL", itemType: "link" },
@@ -74,7 +90,11 @@ export default function Insights() {
     { key: "transferSize", text: "Transfer Size", itemType: "bytes" },
     { key: "resourceSize", text: "Resource Size", itemType: "bytes" },
     { key: "minified", text: "Sctipt Minified", itemType: "binary" },
-    { key: "unusedPercentage", text: "Unused Percentage", itemType: "percentage" },
+    {
+      key: "unusedPercentage",
+      text: "Unused Percentage",
+      itemType: "percentage",
+    },
   ];
 
   async function downloadReport() {
@@ -135,17 +155,13 @@ export default function Insights() {
                 <h4>Analysis Time: {config.waitTime} ms</h4>
               </div>
 
-              {getThirdPartyData().map((item) => {
+              {thirdPartyWithNetwork.map((item, idx) => {
                 return (
-                  item.entityName && (
-                    <div key={item.entityName.name}>
+                    <div key={idx}>
                       <h1 style={{ textAlign: "center" }}>
                         {item.entityName.name}
                       </h1>
-                      <div
-                        className="table-container"
-                        
-                      >
+                      <div className="table-container">
                         <Table
                           id={item.entityName.name}
                           headings={headings}
@@ -155,19 +171,26 @@ export default function Insights() {
                           showPagination={false}
                         />
                       </div>
-                      <div style={{ marginTop: "1em", textAlign: "center", marginBottom: "10em" }} >
-                      <h4> What You Can Do: </h4>
-                      {item.opportunities.user.map((opportunity, idx) => {
-                        return (<p key={idx}>{opportunity}</p>)
-                      })}
-                      <h4> What {item.entityName.name} Can Do: </h4>
-                      {item.opportunities.thirdParty.map((opportunity, idx) => {
-                        return (<p key={idx}>{opportunity}</p>)
-                      })}
+                      <div
+                        style={{
+                          marginTop: "1em",
+                          textAlign: "center",
+                          marginBottom: "10em",
+                        }}
+                      >
+                        <h4> What You Can Do: </h4>
+                        {item.opportunities.user.map((opportunity, idx) => {
+                          return <p key={idx}>{opportunity}</p>;
+                        })}
+                        <h4> What {item.entityName.name} Can Do: </h4>
+                        {item.opportunities.thirdParty.map(
+                          (opportunity, idx) => {
+                            return <p key={idx}>{opportunity}</p>;
+                          }
+                        )}
                       </div>
                     </div>
                   )
-                );
               })}
             </div>
             <div id="editor"></div>
