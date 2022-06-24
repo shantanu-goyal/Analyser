887d4454d634a08015766ea88629981bd45680c7
Shantanu Goyal <shantanu.goyal@sprinklr.com>
2022-06-25 00:04:19 +0530

    Updated Select Box

---
+++ src/components/Select.js
@@ -0,0 +1,9 @@
+import "../styles/Select.css"
+
+function Select({onChange, children, value, placeholder}){
+    return <select className="select-tag" onChange={onChange} value={value} placeholder={placeholder}>
+        {children}
+    </select>
+}
+
+export default Select;
