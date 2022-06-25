9f9d3811d463b2c8efa1a0e508575700d745ef31
Ayush Kumar Gupta <ayush.gupta@sprinklr.com>
2022-06-25 12:02:10 +0530

    Create Modal component

---
+++ src/components/Modal.js
@@ -0,0 +1,25 @@
+import { useState } from "react";
+import "../styles/Modal.css";
+
+function Modal({ name, children }) {
+  const [visible, setVisible] = useState(true);
+
+  function toggleVisibility() {
+    setVisible(!visible);
+  }
+
+  return (
+    <>
+      <button onClick={toggleVisibility} className="modal-button">{name}</button>
+      {visible && (
+        <div className="modal">
+          <div className="modal-content">
+            <img src='close.png' className="modal-close" alt="Close Modal" onClick={toggleVisibility}/>
+            {children}</div>
+        </div>
+      )}
+    </>
+  );
+}
+
+export default Modal;
