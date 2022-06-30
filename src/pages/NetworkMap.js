e195a73bba3c58f1f096fb88d3b44a4d39132159
Ayush Kumar Gupta <ayush.gupta@sprinklr.com>
2022-06-30 11:22:50 +0530

    Add label for nodes

--- src/pages/NetworkMap.js
+++ src/pages/NetworkMap.js
@@ -5,6 +5,7 @@ import ThemeButton from '../components/ThemeButton';
 import Title from '../components/Title';
 import { DataContext } from '../contexts/DataContext';
 
+
 function NetworkMap() {
     const dataContext = useContext(DataContext);
     let data = dataContext.data.data;
@@ -19,7 +20,7 @@ function NetworkMap() {
         const nodes = entriesArray.map(entity => {
             return {
                 id: entity,
-                name: entity
+                name: entity.length > 100 ? entity.substring(0, 100) + '...' : entity
             }
         })
         const edges = data.map(element => {
@@ -38,7 +39,6 @@ function NetworkMap() {
             .linkCurvature('curvature')
             .linkColor(() => 'steelblue')
             .linkDirectionalArrowLength(6)
-            .nodeLabel('id')
             .onNodeDragEnd(node => {
                 node.fx = node.x;
                 node.fy = node.y;
@@ -46,7 +46,6 @@ function NetworkMap() {
                 Graph.centerAt(node.x, node.y, 1000);
                 Graph.zoom(8, 2000);
             });
-
 
         Graph.d3Force('center', null);
 
