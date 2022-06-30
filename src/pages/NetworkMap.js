8dfcbd6412954171ff24b42f14380be1a16a76ea
Shantanu Goyal <shantanu.goyal@sprinklr.com>
2022-06-30 10:30:58 +0530

    Added Search Feature

--- src/pages/NetworkMap.js
+++ src/pages/NetworkMap.js
@@ -1,5 +1,5 @@
 import ForceGraph from 'force-graph';
-import { useContext, useEffect } from 'react';
+import { useContext, useEffect, useState, useCallback } from 'react';
 import { NavBar } from '../components/NavBar';
 import ThemeButton from '../components/ThemeButton';
 import Title from '../components/Title';
@@ -9,9 +9,37 @@ function NetworkMap() {
     const dataContext = useContext(DataContext);
     let data = dataContext.data.data;
     data = data['request-initiators'];
+    let [graphData, setGraphData] = useState(data);
+
+    const debounce = (func) => {
+        let timer;
+        return function (...args) {
+            const context = this;
+            if (timer) clearTimeout(timer);
+            timer = setTimeout(() => {
+                timer = null;
+                func.apply(context, args);
+            }, 500);
+        };
+    };
+
+    const optimizedFn = useCallback(debounce(handleChange), []);
+
+
+
+    function handleChange(value) {
+        let newData = data.filter(element => {
+            return (
+                (element.url.toLowerCase().indexOf(value.toLowerCase()) !== -1) || (element.initiator.toLowerCase().indexOf(value.toLowerCase()) !== -1)
+            )
+        })
+        console.log(newData);
+        setGraphData(newData);
+    }
+
     useEffect(() => {
         const urlMap = new Map();
-        data.forEach(element => {
+        graphData.forEach(element => {
             urlMap.set(element.url, 1);
             urlMap.set(element.initiator, 1);
         });
@@ -22,7 +50,7 @@ function NetworkMap() {
                 name: entity
             }
         })
-        const edges = data.map(element => {
+        const edges = graphData.map(element => {
             return {
                 source: element.initiator,
                 target: element.url
@@ -50,13 +78,22 @@ function NetworkMap() {
 
         Graph.d3Force('center', null);
 
-    })
+    }, [graphData]);
     return <>
         <NavBar />
         <div className="tog-container">
             <ThemeButton>Toggle Dark Mode</ThemeButton>
         </div>
         <Title heading={"Network Map"}></Title>
+        <div className='table-container'>
+            <label htmlFor='search-network-graph'> Search:{" "}
+                <input onChange={(e)=>{
+                    optimizedFn(e.target.value)
+                }} id='search-network-graph' />
+            </label>
+        </div>
+
+
         <div id="network-graph-container">
 
         </div>
